29216ac1f2af8bded6e039c3fe3ed966696c9c37
vikasRanjann <vikash.ranjan933.com>
2022-06-06 20:39:29 +0530

    changes

---
+++ utils.js
@@ -0,0 +1,30 @@
+function sendSuccess(data){
+     return{
+          status: 'sucess', 
+          data: data
+     }
+}
+
+function sendError(error){
+     return{
+          status: 'error', 
+          error: error
+     }
+}
+
+function sendResult(error,data){
+     if(error)
+     {
+          return sendError(error)
+     }
+     else
+     {
+          return sendSuccess(data)
+     }
+
+}
+module.exports = {
+     sendSuccess : sendSuccess,
+     sendError : sendError,
+     sendResult: sendResult
+}
